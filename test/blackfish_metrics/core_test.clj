01f66829f4f86e24889f2a5e92b56dd1e4fb76c2
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-10-17 02:38:21 +0200

    Initial commit

---
+++ test/blackfish_metrics/core_test.clj
@@ -0,0 +1,7 @@
+(ns blackfish-metrics.core-test
+  (:require [clojure.test :refer :all]
+            [blackfish-metrics.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
