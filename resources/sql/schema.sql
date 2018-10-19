1b76fc2f0ffce077b4feef71187f1cbb7b848757
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-10-19 03:55:46 +0200

    Update psql schema: add fifo, prices ex and inc vta

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -6,28 +6,33 @@ create table sales (
 );
 --
 create table items (
-  id integer primary key,
-  created_at timestamp not null,
-  sku varchar(255) not null,
-  description varchar(255) not null,
-  msrp integer not null,
-  online_price integer not null,
+  id            integer primary key,
+  created_at    timestamp not null,
+  sku           varchar(255) not null,
+  description   varchar(255) not null,
+  msrp          integer not null,
+  online_price  integer not null,
   default_price integer not null
 );
 --
 create table sale_lines (
-  id integer primary key,
-  sale_id integer references sales not null,
-  item_id integer references items not null,
+  id         integer primary key,
+  sale_id    integer references sales not null,
+  item_id    integer references items not null,
   created_at timestamp not null,
 
-  unit_qty integer not null default 1,
+  qty        integer not null default 1,
   unit_price integer not null,
-  price integer not null,
-  discount integer
+  total      integer not null,
+  subtotal   integer not null,
+  fifo_price integer not null,
+  discount   integer
 );
 --
 create view sale_lines_with_prices as
   select l.*, i.default_price, i.msrp, i.online_price
   from sale_lines l
-  join items i on l.item_id = i.id;
+  join items i on l.item_id = i.id
+  join sales s on l.sale_id = s.id
+  where s.completed = true;
+--
