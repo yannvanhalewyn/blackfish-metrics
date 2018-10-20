80f5e1fedc7ed6e42aef847eceb71fa3e6dab543
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-10-20 17:55:31 +0200

    Log dates with amsterdam timezone

--- src/blackfish_metrics/logging.clj
+++ src/blackfish_metrics/logging.clj
@@ -1,9 +1,11 @@
 (ns blackfish-metrics.logging
   (:require [clj-time.format :as f]
+            [clj-time.core :as t]
             [clojure.string :as str]))
 
 (defn info [& msgs]
   (println
    (format "[%s] INFO %s"
-           (f/unparse (f/formatter "YYYY-MM-dd hh:mm:ss") (clj-time.core/now))
+           (f/unparse (f/formatter "YYYY-MM-dd HH:mm:ss" (t/time-zone-for-id "Europe/Amsterdam"))
+                      (t/now))
            (str/join " " msgs))))
