990f69d9a74a74e7553094119bded5bb69595cf9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-10-23 05:09:11 +0200

    Create a view with all relations for ease

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -36,17 +36,22 @@ create table manufacturers (
   id   integer primary key,
   name varchar(255)
 );
---
 create table categories (
   id     integer primary key,
   name   varchar(255),
   gender varchar(255)
 );
 --
-create view sale_lines_with_prices as
-  select l.*, i.default_price, i.msrp, i.online_price
+create view sale_lines_with_relations as
+  select l.*, i.default_price, i.msrp, i.online_price,
+    l.subtotal - l.fifo_price * l.qty as profit,
+    case when m.name is null then 'unknown' else m.name end as manufacturer_name,
+    case when c.name is null then 'unknown' else c.name end as category_name,
+    case when c.gender is null then 'unknown' else c.gender end as gender
   from sale_lines l
   join items i on l.item_id = i.id
   join sales s on l.sale_id = s.id
+  left outer join categories c on i.category_id = c.id
+  left outer join manufacturers m on i.manufacturer_id = m.id
   where s.completed = true;
 --
